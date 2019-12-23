d51acd72890158c424092b490973ed758a4a71c1
Kasper Sacharias Eenberg <kse@clearhaus.com>
2019-12-23 08:12:23 +0100

    Initial implementation of endChallenge handler.

--- cmd/agnosco/main.go
+++ cmd/agnosco/main.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
+
 	"fmt"
 	"io/ioutil"
 	"net/http"
@@ -35,6 +38,8 @@ const (
 func main() {
 	kingpin.Parse()
 
+	gin.SetMode(gin.ReleaseMode)
+
 	router := gin.Default()
 
 	// Host e.g. static css and javascript files.
@@ -46,6 +51,7 @@ func main() {
 	router.GET("/", indexHandler)
 	router.POST("/submit", submitHandler)
 	router.POST("/3dsmethod", threeDSMethodHandler)
+	router.POST("/endchallenge", challengeEndHandler)
 
 	address := fmt.Sprintf("localhost:%d", *port)
 	fmt.Printf("Connect to http://%s\n", address)
@@ -62,6 +68,7 @@ func threeDSMethodHandler(ctx *gin.Context) {
 	input, ok := ctx.GetPostForm("input")
 	if !ok {
 		ctx.Status(http.StatusBadRequest)
+		return
 	}
 
 	body, e := apiCall(MethodPreAuth, input)
@@ -76,6 +83,7 @@ func submitHandler(ctx *gin.Context) {
 	input, ok := ctx.GetPostForm("input")
 	if !ok {
 		ctx.Status(http.StatusBadRequest)
+		return
 	}
 
 	body, e := apiCall(MethodAuth, input)
@@ -84,6 +92,79 @@ func submitHandler(ctx *gin.Context) {
 	} else {
 		ctx.String(http.StatusOK, body)
 	}
+}
+
+func challengeEndHandler(ctx *gin.Context) {
+	var output = make(map[string]interface{})
+
+	// 1. Receive final CRes
+	cresB64, ok := ctx.GetPostForm("cres")
+	if !ok {
+		// TODO: An error should be returned.
+		ctx.String(http.StatusBadRequest, "No cres in post form")
+		return
+	}
+
+	cresJSON, e := base64.RawURLEncoding.DecodeString(cresB64)
+	if e != nil {
+		ctx.String(http.StatusBadRequest, "Invalid cres Base64 %s", e.Error())
+		return
+	}
+
+	// 2. If not error: Fetch RReq from EDSS
+	M := make(map[string]interface{})
+	e = json.Unmarshal(cresJSON, &M)
+	if e != nil {
+		ctx.String(http.StatusBadRequest, "Invalid cres JSON: %s", e.Error())
+		return
+	}
+
+	messageType, ok := getString(M, "messageType")
+	if e != nil {
+		ctx.String(http.StatusBadRequest, "No messageType in returned JSON: %s", cresJSON)
+		return
+	}
+
+	switch messageType {
+	case "Erro":
+		output["Erro"] = M
+		ctx.JSON(http.StatusOK, output)
+		return
+	case "CRes":
+		output["CRes"] = M
+	default:
+		ctx.String(http.StatusBadRequest, "Unexpected messageType: %s", cresJSON)
+		return
+	}
+
+	threeDSServerTransID, ok := getString(M, "threeDSServerTransID")
+	if e != nil {
+		ctx.String(http.StatusBadRequest, "Expected threeDSServerTransID: %s", cresJSON)
+		return
+	}
+
+	// From now on we can redirect to the correct listener, if the ID is valid.
+
+	transStatus, ok := getString(M, "transStatus")
+	if e != nil {
+		ctx.String(http.StatusBadRequest, "Expected transStatus: %s", cresJSON)
+		return
+	}
+
+	var rreq []byte
+	if contains([]string{"Y", "A"}, transStatus) {
+		rreq, e = fetchRReq(threeDSServerTransID)
+		if e != nil {
+			output["Erro"] = fmt.Sprintf("Error fetching RReq: %s", e.Error())
+		} else {
+			output["RReq"] = rreq
+		}
+
+		fmt.Printf("%s\n", rreq)
+	}
+
+	ctx.JSON(http.StatusOK, output)
+	// 3. Notify of success, somehow.
 }
 
 func edssURL(method APIMethod) (base string) {
@@ -135,4 +216,40 @@ func apiCall(method APIMethod, input string) (response string, e error) {
 	response = string(respBody)
 
 	return
+}
+
+func fetchRReq(threeDSServerTransID string) (rreq []byte, e error) {
+	inputStruct := struct {
+		ThreeDSServerTransID string `json:"threeDSServerTransID"`
+	}{
+		threeDSServerTransID,
+	}
+
+	input, _ := json.Marshal(inputStruct)
+
+	response, e := apiCall(MethodPostAuth, string(input))
+	rreq = []byte(response)
+
+	return
+}
+
+func getString(M map[string]interface{}, key string) (val string, ok bool) {
+	T, ok := M[key]
+	if !ok {
+		return
+	}
+
+	val, ok = T.(string)
+
+	return
+}
+
+func contains(list []string, value string) bool {
+	for _, el := range list {
+		if el == value {
+			return true
+		}
+	}
+
+	return false
 }
